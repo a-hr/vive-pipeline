d3381127089a47562aa3566734706468766bfff2
a-hr <heerreroalvaro@gmail.com>
2024-05-24 13:03:49 +0200

    updated launch_script

--- launch_cluster.sh
+++ launch_cluster.sh
@@ -1,34 +1,19 @@
 #!/usr/bin/env bash
 
-#SBATCH --time=20:00:00
-#SBATCH --mem=8G
+#SBATCH --time=1:00:00
+#SBATCH --mem=4G
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=nextflow-cDNA-fw_new
+#SBATCH --output=logs/%x-%j.out
 
-module load Singularity Nextflow Go Mamba
+module load Singularity Go Nextflow 
+mkdir -p logs
 
 # limit the resources of the JVM
-export NXF_OPTS="-Xms500M -Xmx8G"
+export NXF_OPTS="-Xms500M -Xmx4G"
+
+# set the log directory
+export NXF_LOG="$PWD/logs"
 
 # launch the main process
-nextflow run main.nf -profile cluster \
-    --run_name "vive-jul23_cDNA-pipeline" \
-    --input_fastq "inputs-0/*.fastq*" \
-    --output_dir "output-jul23_cDNA" \
-    --is_rna false
-
-nextflow run main.nf -profile cluster \
-    --run_name "vive-dec23_cDNA-pipeline" \
-    --input_fastq "inputs-1/*.fastq*" \
-    --output_dir "output-dec23_cDNA" \
-    --is_rna false
-
-nextflow run main.nf -profile cluster \
-    --run_name "vive-jul23_dRNA-pipeline" \
-    --input_fastq "inputs-2/*.fastq*" \
-    --output_dir "output-jul23_dRNA" \
-    --is_rna true
-
-nextflow run main.nf -profile cluster \
-    --run_name "vive-dec23_dRNA-pipeline" \
-    --input_fastq "inputs-3/*.fastq*" \
-    --output_dir "output-dec23_dRNA" \
-    --is_rna true
+nextflow run main.nf -profile cluster -params-file inputs/params/input_params_cDNA_fw.yaml -resume
